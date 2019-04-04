6dfe31150432813e9156f486b4ae0cb8cb21deb7
Henrique de Almeida <henriquealmeida1492@gmail.com>
2019-04-04 14:34:50 -0300

    feat: capture photo in service with no preview, send to activity

---
+++ app/src/main/java/com/example/henriqueribeirodealmeida/observador/CamService.java
@@ -0,0 +1,122 @@
+package com.example.henriqueribeirodealmeida.observador;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.androidhiddencamera.CameraConfig;
+import com.androidhiddencamera.CameraError;
+import com.androidhiddencamera.HiddenCameraService;
+import com.androidhiddencamera.HiddenCameraUtils;
+import com.androidhiddencamera.config.CameraFacing;
+import com.androidhiddencamera.config.CameraImageFormat;
+import com.androidhiddencamera.config.CameraResolution;
+
+import java.io.File;
+
+public class CamService extends HiddenCameraService {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            if (HiddenCameraUtils.canOverDrawOtherApps(this)) {
+                CameraConfig cameraConfig = new CameraConfig()
+                        .getBuilder(this)
+                        .setCameraFacing(CameraFacing.REAR_FACING_CAMERA)
+                        .setCameraResolution(CameraResolution.MEDIUM_RESOLUTION)
+                        .setImageFormat(CameraImageFormat.FORMAT_JPEG)
+                        .build();
+
+                startCamera(cameraConfig);
+
+                new android.os.Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(CamService.this,
+                                "Capturing image.", Toast.LENGTH_SHORT).show();
+
+                        takePicture();
+                    }
+                }, 2000L);
+            } else {
+
+                //Open settings to grant permission for "Draw other apps".
+                HiddenCameraUtils.openDrawOverPermissionSetting(this);
+            }
+        } else {
+
+            //TODO Ask your parent activity for providing runtime permission
+            Toast.makeText(this, "Camera permission not available", Toast.LENGTH_SHORT).show();
+        }
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onImageCapture(@NonNull File imageFile) {
+        Toast.makeText(this,
+                "Captured image size is : " + imageFile.length(),
+                Toast.LENGTH_SHORT)
+                .show();
+
+        // Do something with the image...
+        String filePath = imageFile.getPath();
+        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+
+        Intent intent = new Intent("sendPictureToActivity");
+        Bundle b = new Bundle();
+        b.putParcelable("picture", bitmap);
+        intent.putExtra("bundle", b);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+
+        stopSelf();
+    }
+
+    @Override
+    public void onCameraError(@CameraError.CameraErrorCodes int errorCode) {
+        switch (errorCode) {
+            case CameraError.ERROR_CAMERA_OPEN_FAILED:
+                //Camera open failed. Probably because another application
+                //is using the camera
+                Toast.makeText(this, "Can't open", Toast.LENGTH_LONG).show();
+                break;
+            case CameraError.ERROR_IMAGE_WRITE_FAILED:
+                //Image write failed. Please check if you have provided WRITE_EXTERNAL_STORAGE permission
+                Toast.makeText(this, "Can't write", Toast.LENGTH_LONG).show();
+                break;
+            case CameraError.ERROR_CAMERA_PERMISSION_NOT_AVAILABLE:
+                //camera permission is not available
+                //Ask for the camera permission before initializing it.
+                Toast.makeText(this, "Can't get permission", Toast.LENGTH_LONG).show();
+                break;
+            case CameraError.ERROR_DOES_NOT_HAVE_OVERDRAW_PERMISSION:
+                //Display information dialog to the user with steps to grant "Draw over other app"
+                //permission for the app.
+                HiddenCameraUtils.openDrawOverPermissionSetting(this);
+                break;
+            case CameraError.ERROR_DOES_NOT_HAVE_FRONT_CAMERA:
+                Toast.makeText(this, "There's no camera", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        stopSelf();
+    }
+}
--- app/src/main/java/com/example/henriqueribeirodealmeida/observador/MainActivity.java
+++ app/src/main/java/com/example/henriqueribeirodealmeida/observador/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.henriqueribeirodealmeida.observador;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.androidhiddencamera.HiddenCameraFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private HiddenCameraFragment mHiddenCameraFragment;
+    private ImageView pictureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pictureView = findViewById(R.id.picture);
+
+        Button takePictureButton = findViewById(R.id.take_picture);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mHiddenCameraFragment != null) {    //Remove fragment from container if present
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .remove(mHiddenCameraFragment)
+                            .commit();
+                    mHiddenCameraFragment = null;
+                }
+
+                startService(new Intent(MainActivity.this, CamService.class));
+            }
+        });
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver, new IntentFilter("sendPictureToActivity"));
     }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Bundle bundle = intent.getBundleExtra("bundle");
+            Bitmap image = bundle.getParcelable("picture");
+
+            pictureView.setImageBitmap(image);
+        }
+    };
 }
